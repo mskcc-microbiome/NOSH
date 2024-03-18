dbb22adc985c41d78cf5845691d739a298fb1863
Nick Waters <nickp60@users.noreply.github.com>
2024-03-18 16:04:49 -0400

    add dashboard and patient-level dashboard modules

---
+++ R/mod_dashboard.R
@@ -0,0 +1,32 @@
+mod_nutrient_select_ui <- function(id) {
+  selectInput(NS(id, 'nutrients'), label = "Select Nutrient to Display", choices = colnames(fndds_summary)[5:ncol(fndds_summary)], selected = "protein_g", multiple = TRUE)
+}
+mod_dashboard_ui <- function(id) {
+  plotOutput(NS(id, 'summary'))
+}
+mod_dashboard_server <- function(id) {
+  moduleServer(id, function(input, output, session) {
+    pt_data_full_merge <- merge_meals_and_units(dev_data = dev_data, unittable = unittable, fndds_summary = fndds_summary)
+    get_plot_data <- reactive({
+      tabulate_pt_nutrition(pt_data_full_merge, mrn=3, nutrient_list=input$nutrients,
+                            dt_start="1914-06-21", dt_end="1914-06-21") %>% 
+        group_by(meal, date_intake, nutrient) %>% 
+        summarize(daily_total=sum(consumed_value))
+    })
+    output$summary <- renderPlot({ 
+      ggplot(get_plot_data() , aes(x=interaction(date_intake, meal), color=nutrient, y=daily_total)) + geom_point()
+    }
+    )
+  })
+}
+
+
+mod_dashboard_demo <- function() {
+  ui <- fluidPage(mod_nutrient_select_ui("taco"), mod_dashboard_ui("taco"))
+  server <- function(input, output, session) {
+    mod_dashboard_server("taco")
+  }
+  shinyApp(ui, server)
+  
+}
+mod_dashboard_demo()
